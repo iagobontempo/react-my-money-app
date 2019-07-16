513109a923f9ad4f3af1936e1f45f9e36503e3f7
Iago Bontempo <iagobontempo@hotmail.com>
2019-07-16 11:47:00 -0300

    Backend, fix pequeno problema sintaxe

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -1,7 +1,7 @@
 const BillingCycle = require('./billingCycle') //Pegou a referencia da Database e Schemas
 
 BillingCycle.methods(['get', 'post', 'put', 'delete']) //Metodos que utilizarei nestes esquemas
-BillingCycle.uptadeOptions({new: true, runValidators: true}) 
+BillingCycle.updateOptions({new: true, runValidators: true}) 
 // NEW - Opção para sempre que for atualizado, mostrar o objeto novo
 // RUNVALIDATORS - opção para usar validators em todos os metodos (ex: os campos required: true, na db)
 
