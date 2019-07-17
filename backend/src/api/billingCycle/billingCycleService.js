5683a9798b060cd68bfd14693dada09637339350
Iago Bontempo <iagobontempo@hotmail.com>
2019-07-17 09:08:26 -0300

    Backend, objento quantidade de registro (count)

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -5,4 +5,17 @@ BillingCycle.updateOptions({new: true, runValidators: true})
 // NEW - Opção para sempre que for atualizado, mostrar o objeto novo
 // RUNVALIDATORS - opção para usar validators em todos os metodos (ex: os campos required: true, na db)
 
+//Configuração de um contador
+// Como a rota billingCycle já esta registrada em routes, não é nescessario registrar isso denovo.
+// Lembrando que o 'count' esta dentro de billingCycle, entao a rota seria api/billingCycles/count
+BillingCycle.route('count', (req, res, next) => {
+    BillingCycle.count((error, value) => {
+        if(error) {
+            res.status(500).json({errors: [error]}) //Colocando erro em um objeto json
+        } else {
+            res.json({value}) // O value seria a quantidade de objetos que existe dentro de billingCycle
+        }
+    })
+})
+
 module.exports = BillingCycle
--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,3 +2,8 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise 
 
 module.exports = mongoose.connect('mongodb://localhost/mymoney')
+
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatorio." //Apenas mensagem de erro caso nào atenda os requires na hora do post.
+mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite minimo de '{MIN}'." 
+mongoose.Error.messages.Number.max = "O '{VALUE}' informado é maior que o limite maximo de '{MAX}'." 
+mongoose.Error.messages.String.enum = "'{VALUE}' não é valido para o atributo '{PATH}'." 
