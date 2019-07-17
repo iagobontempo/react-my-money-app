718ef0c393d7a127b385676c9a187468b7419bea
Iago Bontempo <iagobontempo@hotmail.com>
2019-07-17 10:23:13 -0300

    Backend, comentario cors

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-    res.header('Acess-Control-Allow-Origin', '*')
+    res.header('Acess-Control-Allow-Origin', '*') //com * significa que permite todas 
     res.header('Acess-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
     res.header('Acess-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     next()
