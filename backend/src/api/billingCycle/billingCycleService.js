55f4e7f38977a76ebc10995d10bc9d93ab154f6d
Iago Bontempo <iagobontempo@hotmail.com>
2019-07-17 10:18:00 -0300

    Backend, errorHandlers

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -1,9 +1,11 @@
 const BillingCycle = require('./billingCycle') //Pegou a referencia da Database e Schemas
+const errorHandler = require('../common/errorHandler')
 
 BillingCycle.methods(['get', 'post', 'put', 'delete']) //Metodos que utilizarei nestes esquemas
 BillingCycle.updateOptions({new: true, runValidators: true}) 
 // NEW - Opção para sempre que for atualizado, mostrar o objeto novo
 // RUNVALIDATORS - opção para usar validators em todos os metodos (ex: os campos required: true, na db)
+BillingCycle.after('post', errorHandler).after('put', errorHandler) // Unindo errorHandler, com metodos
 
 //Configuração de um contador
 // Como a rota billingCycle já esta registrada em routes, não é nescessario registrar isso denovo.
