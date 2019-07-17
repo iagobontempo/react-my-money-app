ed4418ae7057d51b98f0b575ab811ab026d3ff08
Iago Bontempo <iagobontempo@hotmail.com>
2019-07-17 16:06:12 -0300

    Limpando

---
+++ frontend/src/main/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../common/template/dependencies'
+
+import Header from '../common/template/header'
+
+function App() {
+    return (
+      <div className="wrapper">
+        <Header />
+      </div>
+    );
+  }
+
+export default App;
