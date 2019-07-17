5565d310554cfa6f689bdb66f03137392e9dde15
Iago Bontempo <iagobontempo@hotmail.com>
2019-07-17 16:50:26 -0300

    Frontend, componentes menu, menuItem, menuTree e footer

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default props => (
+    <footer className="main-footer">
+        <strong>Copyright &Copy; 2019</strong>
+        <a href="http://google.com" target="_blank"> Iago Bontempo </a>
+    </footer>
+)
