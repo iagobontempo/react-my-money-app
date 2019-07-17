585ca7e9ca19700882608d520983e9d93296f36e
Iago Bontempo <iagobontempo@hotmail.com>
2019-07-17 10:22:44 -0300

    Backend, habilitando cors (permite que certas urls usar os dados, no caso permitindo todas)

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -2,11 +2,13 @@ const port = 3003
 
 const bodyParser = require('body-parser')
 const express = require('express')
+const allowCors = require('./cors')
 
 const server = express() // Sempre que chamar o express() ele ira chamar um novo servidor
 
 server.use(bodyParser.urlencoded({ extended: true })) //Para toda requisão use o bodyparser para requisicoes urlencoded
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port, function () {
     console.log(`BACKEND RUNNING ON PORT ${port}`)
