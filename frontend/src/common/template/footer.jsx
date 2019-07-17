8e243058a1bad0883da02b5072a6bbbf1b6e9dfd
Iago Bontempo <iagobontempo@hotmail.com>
2019-07-17 17:46:22 -0300

    Frontend, configurando redux

--- frontend/src/common/template/footer.jsx
+++ frontend/src/common/template/footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default props => (
     <footer className="main-footer">
-        <strong>Copyright &Copy; 2019</strong>
+        <strong>Copyright @ 2019</strong>
         <a href="http://google.com" target="_blank"> Iago Bontempo </a>
     </footer>
 )
