7050223683234e780925ca9068924d0eb5f0f925
Iago Bontempo <iagobontempo@hotmail.com>
2019-07-17 19:39:38 -0300

    Frontend, dashboardReducer e integrando inicial state

--- frontend/src/pages/dashboard/dashboard.jsx
+++ frontend/src/pages/dashboard/dashboard.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 import ContentHeader from '../../common/template/contentHeader'
 import Content from '../../common/template/content'
@@ -7,14 +8,18 @@ import Row from '../../common/layout/row' // USADO APENAS PARA ALINHAR OS ValueB
 
 class Dashboard extends Component {
     render() {
+        const { credit, debt } = this.props.summary
         return (
             <div>
                 <ContentHeader tittle="Dashboard" small="Versao 1.0" />
                 <Content>
                     <Row>
-                    <ValueBox cols="12 4" color="green" icon="bank" value="R$ 10,00" text="Total de creditos" />
-                    <ValueBox cols="12 4" color="red" icon="credit-card" value="R$ 10,00" text="Total de débitos" />
-                    <ValueBox cols="12 4" color="blue" icon="money" value="R$ 0,00" text="Valor consolidado" />
+                    <ValueBox cols="12 4" color="green" icon="bank" 
+                        value={`R$ ${credit}`} text="Total de creditos" />
+                    <ValueBox cols="12 4" color="red" icon="credit-card" 
+                        value={`R$ ${debt}`} text="Total de débitos" />
+                    <ValueBox cols="12 4" color="blue" icon="money" 
+                        value={`R$ ${credit - debt}`} text="Valor consolidado" />
                     </Row>
                 </Content>
             </div>
@@ -22,4 +27,7 @@ class Dashboard extends Component {
     }
 }
 
-export default Dashboard
+const mapStateToProps = state => ({
+  summary: state.dashboard.summary
+});
+export default connect(mapStateToProps)(Dashboard)
