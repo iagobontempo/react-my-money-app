70254df4ec8afe3352003beb61e91813ff60d87c
Iago Bontempo <iagobontempo@hotmail.com>
2019-07-18 08:55:41 -0300

    Frontend, dashboard sem redux

--- frontend/src/common/template/menu.jsx
+++ frontend/src/common/template/menu.jsx
@@ -5,6 +5,7 @@ import MenuTree from './menuTree'
 export default props =>(
     <ul className="sidebar-menu">
         <MenuItem path="/" label="Dashboard" icon="dashboard"/>
+        <MenuItem path="#/dashboard2" label="Dashboard v2" icon="dashboard"/>
         <MenuTree label="Cadastro" icon="edit">
             <MenuItem path="#/billingCycles" label="Ciclos de pagamento" icon="usd" />
         </MenuTree>
