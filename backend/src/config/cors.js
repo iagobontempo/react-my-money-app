dd39941111a94da23f41bb7bde6918220dad24fe
Iago Bontempo <iagobontempo@hotmail.com>
2019-07-18 11:21:01 -0300

    Backend, CORS fix

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
-    res.header('Acess-Control-Allow-Origin', '*') //com * significa que permite todas 
-    res.header('Acess-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-    res.header('Acess-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    res.header('Access-Control-Allow-Origin', '*') //com * significa que permite todas 
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     next()
 }
