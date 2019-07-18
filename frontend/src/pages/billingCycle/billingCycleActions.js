c514387a68fab4b84ce913878485cca76f3fd51b
Iago Bontempo <iagobontempo@hotmail.com>
2019-07-18 17:21:31 -0300

    Frontend, melhorias no Ciclo de pagamentos

--- frontend/src/pages/billingCycle/billingCycleActions.js
+++ frontend/src/pages/billingCycle/billingCycleActions.js
@@ -1,5 +1,9 @@
 import axios from 'axios'
 import { toastr } from 'react-redux-toastr' //Importou para pegar os metodos que ele já possui (ex: success)
+import { reset as resetForm } from 'redux-form'
+import { selectTab, showTabs } from '../../common/tab/tabActions'
+import billingCycleForm from './billingCycleForm';
+
 const BASE_URL = 'http://localhost:3003/api'
 
 export function getList() {
@@ -11,14 +15,20 @@ export function getList() {
 }
 
 export function create(values) {
-    axios.post(`${BASE_URL}/billingCycles`, values)
-        .then(resp => {
-            toastr.success('Sucesso', 'Operação realizada com sucesso.')
-        })
-        .catch(e => {
-            e.response.data.errors.forEach(error => toastr.error('Erro', error))
-        })
-    return {
-        type: 'TEMP'
+    return dispatch => {
+        axios.post(`${BASE_URL}/billingCycles`, values)
+            .then(resp => {
+                toastr.success('Sucesso', 'Operação realizada com sucesso.')
+                dispatch([ // Só é possivel chamar 4 ações por conta do MULTI
+                    resetForm('billingCycleForm'),
+                    getList(),
+                    selectTab('tabList'),
+                    showTabs('tabList', 'tabCreate')
+                ])
+            })
+            .catch(e => {
+                e.response.data.errors.forEach(error => toastr.error('Erro', error))
+            })
+
     }
 }
