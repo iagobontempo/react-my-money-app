602dae9faeb3aaa0d238e2ce5abf3dc75a0e49c4
Iago Bontempo <iagobontempo@hotmail.com>
2019-07-18 10:04:46 -0300

    Frontend, Connectando billingCycle com redux

--- frontend/src/pages/billingCycle/billingCycle.jsx
+++ frontend/src/pages/billingCycle/billingCycle.jsx
@@ -1,41 +1,49 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 
 import ContentHeader from '../../common/template/contentHeader'
 import Content from '../../common/template/content'
-
 import Tabs from '../../common/tab/tabs'
 import TabsHeader from '../../common/tab/tabsHeader'
 import TabsContent from '../../common/tab/tabsContent'
-
 import TabHeader from '../../common/tab/tabHeader'
 import TabContent from '../../common/tab/tabContent'
 
+import { selectTab } from '../../common/tab/tabActions'
 
 
 class BillingCycle extends Component {
-  render() {
-    return (
-        <div>
-            <ContentHeader tittle="Ciclos de pagamentos" small="Cadastro" />
-            <Content>
-                <Tabs>
-                    <TabsHeader>
-                        <TabHeader label="Listar" icon='bars' target='tabList'/>
-                        <TabHeader label="Incluir" icon='plus' target='tabCreate'/>
-                        <TabHeader label="Alterar" icon='pencil' target='tabUpdate'/>
-                        <TabHeader label="Excluir" icon='trash-o' target='tabDelete'/>
-                    </TabsHeader>
-                    <TabsContent>
-                        <TabContent id="tabList"><h1>Lista</h1></TabContent>
-                        <TabContent id="tabCreate"><h1>Create</h1></TabContent>
-                        <TabContent id="tabUpdate"><h1>Update</h1></TabContent>
-                        <TabContent id="tabDelete"><h1>Delete</h1></TabContent>
-                    </TabsContent>
-                </Tabs>
-            </Content>
-        </div>
-    )
-  }
+
+    componentDidMount() {
+        this.props.selectTab('tabList') // Selecionando tabList como aba da tabela renderizada ao abrir a page
+    }                                   // this.props.selectTab('tabUpdate') abriria a aba update 
+
+    render() {
+        return (
+            <div>
+                <ContentHeader tittle="Ciclos de pagamentos" small="Cadastro" />
+                <Content>
+                    <Tabs>
+                        <TabsHeader>
+                            <TabHeader label="Listar" icon='bars' target='tabList' />
+                            <TabHeader label="Incluir" icon='plus' target='tabCreate' />
+                            <TabHeader label="Alterar" icon='pencil' target='tabUpdate' />
+                            <TabHeader label="Excluir" icon='trash-o' target='tabDelete' />
+                        </TabsHeader>
+                        <TabsContent>
+                            <TabContent id="tabList"><h1>Lista</h1></TabContent>
+                            <TabContent id="tabCreate"><h1>Create</h1></TabContent>
+                            <TabContent id="tabUpdate"><h1>Update</h1></TabContent>
+                            <TabContent id="tabDelete"><h1>Delete</h1></TabContent>
+                        </TabsContent>
+                    </Tabs>
+                </Content>
+            </div>
+        )
+    }
 }
 
-export default BillingCycle
+const mapDispatchToProps = dispatch =>
+    bindActionCreators({ selectTab }, dispatch);
+export default connect(null, mapDispatchToProps)(BillingCycle)
